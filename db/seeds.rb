753a1c55d3919acb71d142560abfa649297311ca
Sean Witt <Sean.robin.witt@gmail.com>
2016-10-03 12:02:41 -0500

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,11 @@
 @type2 = Type.create(name: "Saw")
 @type3 = Type.create(name: "Power Washer")
 
-@hammer = Tool.create(display_name: "Hammer", brand: "acme", model: "The anihilator", type: 1, user_id: 1 )
-@saw = Tool.create(display_name: "Saw", brand: "acme", model: "Turbo Saw 3000", type: 2, user_id: 1 )
-@power_washer = Tool.create(display_name: "Power Washer", brand: "acme", model: "x4753 hybrid flex", type: 3, user_id: 1 )
+@hammer = Tool.create(display_name: "Hammer", make: "acme", model: "The anihilator", type_id: 1, user_id: 1 )
+@saw = Tool.create(display_name: "Saw", make: "acme", model: "Turbo Saw 3000", type_id: 2, user_id: 1 )
+@power_washer = Tool.create(display_name: "Power Washer", make: "acme", model: "x4753 hybrid flex", type_id: 3, user_id: 1 )
 
 @user1 = User.create(name: "Sean Witt", password: "password", email: "sean@sean.com", address: "1783 Ohlen Rd, Austin TX 78757")
 @user2 = User.create(name: "Tom Ho", password: "password", email: "tom@tom.com", address: "1705 Guadalupe, Austin TX 78717")
 
-@user1 << @hammer
-@user1 << @saw
-@user2 << @power_washer
 
