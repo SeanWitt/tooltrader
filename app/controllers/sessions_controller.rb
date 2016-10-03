0011f5f3d17853cd29470ec17aaa276b71874db1
Sean Witt <Sean.robin.witt@gmail.com>
2016-10-03 15:00:37 -0500

    add session controller create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,9 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: session_params[:email])
     if @user && @user.authenticate(password: session_params["password"])
+      p "i got here"
       session[:id] =  @user.id
-      redirect_to user_path(@user)
+      redirect_to user_path(@user.id)
     else
       render 'new'
     end
