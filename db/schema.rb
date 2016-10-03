33c2ae74b9586cb74fcfd62dd7451c5bb9c9a7ea
Sean Witt <Sean.robin.witt@gmail.com>
2016-10-03 12:27:36 -0500

    build simple index view for tools

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161003164725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "tools", force: :cascade do |t|
     t.string   "display_name"
