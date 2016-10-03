9fb989885960dadab6a7abdd978ab63bf2e19378
Sean Witt <Sean.robin.witt@gmail.com>
2016-10-03 11:41:10 -0500

    build user migration

--- db/migrate/20161003153707_tools.rb
+++ db/migrate/20161003153707_tools.rb
@@ -1,4 +1,12 @@
 class Tools < ActiveRecord::Migration[5.0]
   def change
+    create_table :tools do |t|
+      t.string :display_name
+      t.string :make
+      t.string :model
+      t.integer :type_id
+      t.interger :user_id
+      t.timestamps
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@type1 = Type.create(name: "Hammer")
+@type2 = Type.create(name: "Saw")
+@type3 = Type.create(name: "Power Washer")
+
+@hammer = Tool.create(display_name: "Hammer", model: "swinger", type: 1, user_id: 1 )
+@saw = Tool.create(display_name: "Saw", model: "Acme", type: 2, user_id: 1 )
+@power_washer = Tool.create(display_name: "Power Washer", model: "swinger", type: 3, user_id: 1 )
+
+
+    # t.string :display_name
+    #   t.string :make
+    #   t.string :model
+    #   t.string :type
+    #   t.interger :user_id
