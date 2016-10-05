cf709479a2e8447086276f85af13b78cb6517b77
Tom Ho <datomster21@gmail.com>
2016-10-04 19:33:30 -0500

    create feature to create new users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session,
+      if: Proc.new { |c| c.request.format =~ %r{application/json} }
 
   helper_method :current_user
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,21 @@ class UsersController < ApplicationController
     @user = current_user
     @tools = @user.tools
   end
+
+  def new
+  end
+
+  def create
+    p user_params
+    @user = User.create(user_params)
+    @tools = @user.tools
+    p @user.errors
+    session[:id] = @user.id
+    render 'show'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :address, :password )
+  end
 end
