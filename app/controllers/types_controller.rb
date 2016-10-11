a0e4e0bf6c709269eedb3d2242f9697fdf268c1e
Tom Ho <datomster21@gmail.com>
2016-10-11 18:11:27 -0500

    refine search for nearby tools

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -7,12 +7,23 @@ class TypesController < ApplicationController
   def search
     nearby_users = current_user.nearby_users(15)
     @tools = nearby_users.flat_map { |user| user.tools }
-    @tools_nearby = []
+    @tools_nearby = { p1: [], p2:[], p3: [], p4: [], p5: [] }
+    keyword = params[:name].downcase
     @tools.each do |tool|
-      if tool.type.name.downcase == params[:name].downcase
-        @tools_nearby << tool
+      if tool.display_name.downcase.include?(keyword)
+        @tools_nearby[:p1] << tool
+      elsif tool.type.name.downcase.inlcude?(keyword)
+        @tools_nearby[:p2] << tool
+      elsif tool.make.downcase.include?(keyword)
+        @tools_nearby[:p3] << tool
+      elsif tool.tool_model.downcase.inlcude?(keyword)
+        @tools_nearby[:p4] << tool
+      elsif tool.description.downcase.include?(keyword)
+        @tools_nearby[:p5] << tool
       end
     end
+
+    return @tools_nearby.values.flatten
   end
 
 end
