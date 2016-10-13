1e919ed6c2810b61bdda789b389b969057a9bb3f
Sean Witt <Sean.robin.witt@gmail.com>
2016-10-13 16:55:39 -0500

    add working reviews display onn show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @tools = @user.tools
+    @reviews = Review.where(recipient_id: @user.id)
   end
 
   def new
